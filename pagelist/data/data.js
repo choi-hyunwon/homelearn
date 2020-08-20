53901c453712270b7002bcec2069995ec200c5a9
choi hyun won <danboys@domfam.com>
2020-08-20 19:50:35 +0900

    0.3 추가

--- pagelist/data/data.js
+++ pagelist/data/data.js
@@ -101,9 +101,78 @@ data.main = {
     },
     {
       'stateClass': 'complete',
+      'url': '01_main_05_교육과정별 메인_01_초등_01',
+      'category': 'Main',
+      'page': '교육과정'
+    },
+    {
+      'stateClass': 'complete',
       'url': '01_main_06_태그별 전체 목록',
       'category': 'Main',
       'page': '태그별 전체 목록'
     }
+  ]
+}
+
+data.detail = {
+  'title': '상세',
+  'list': [
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_01_로그아웃',
+      'category': 'Main',
+      'page': '로그아웃'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_01_학부모',
+      'category': 'Main',
+      'page': '학부모'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_01_학생',
+      'category': 'Main',
+      'page': '학생'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_01_학습소개_02_scroll',
+      'category': 'Main',
+      'page': '학습소개'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_01_학습소개_로그아웃',
+      'category': 'Main',
+      'page': '학습소개(로그아웃)'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_01_학습중',
+      'category': 'Main',
+      'page': '학습소개(탭메뉴)'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_02_리뷰_01',
+      'category': 'Main',
+      'page': '리뷰(탭메뉴)'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_08_app detail_03_관련 학습',
+      'category': 'Main',
+      'page': '관련학습(탭메뉴)'
+    },
+    {
+      'stateClass': 'complete',
+      'url': '01_main_09_popup_06_평가 및 리뷰 작성',
+      'category': 'Main',
+      'page': '평가하기(팝업)'
+    },
+
+
+
   ]
 }
